5c2d50924bff2e0c863b90b359f76b19e03c649b
Arthur Cohen <arthur.cohen@embecosm.com>
2023-11-15 23:35:56 +0100

    lcu: Add parsing of commits

--- src/main.rs
+++ src/main.rs
@@ -60,16 +60,22 @@
 
 use std::{
     collections::HashSet,
+    env,
     path::PathBuf,
     process::{Command, Stdio},
+    str::Split,
 };
 
+use clap::Parser;
+
+#[derive(Debug)]
 struct ChangelogLine {
     file: PathBuf,
     message: String,
 }
 // format as `\t* <file>: <message>\n`
 
+#[derive(Debug)]
 struct ChangelogEntry {
     file: String,              // PathBuf?
     lines: Vec<ChangelogLine>, // NonEmptyVec?
@@ -77,48 +83,106 @@ struct ChangelogEntry {
 // format as `<file>:\n <lines>`
 
 // first line of a commit
+#[derive(Debug)]
 struct Title(String);
 
 // text between the title and the Changelog
+#[derive(Debug)]
 struct Body(String);
 
 // Signed-off-by line - optional
+#[derive(Debug)]
 struct SoB(String);
 // format as `<0>`
 
+#[derive(Debug)]
 struct Commit {
     title: Title,
-    body: Body,
+    body: Option<Body>,
     changelog_entries: Vec<ChangelogEntry>, // but cull away the empty ChangelogEntries,
     sob: Option<SoB>,
 }
 
 impl Commit {
+    fn parse_changelog_line(line: &str) -> ChangelogLine {
+        // \t* <file>: <msg>
+        let line = line.trim_start_matches(|c| c == '\t' || c == '*' || c == ' ');
+
+        let (file, message) = line.split_once(':').unwrap();
+
+        let file = PathBuf::from(file);
+        let message = message.trim_start().to_string();
+
+        ChangelogLine { file, message }
+    }
+
+    fn maybe_parse_changelog_entry(blocks: &mut Split<'_, &str>) -> Option<ChangelogEntry> {
+        let changelog = blocks.next();
+        let entries = blocks.next();
+
+        let is_changelog_entry =
+            changelog.map(|s| !(s.is_empty() || s.starts_with("Signed-off-by")));
+
+        dbg!(changelog, entries, &is_changelog_entry);
+
+        is_changelog_entry.and_then(|b| {
+            b.then(|| ChangelogEntry {
+                file: changelog.unwrap().split_once(':').unwrap().0.to_string(),
+                lines: entries
+                    .unwrap()
+                    .lines()
+                    .map(Commit::parse_changelog_line)
+                    .collect(),
+            })
+        })
+    }
+
+    fn parse_changelog_entries(blocks: &mut Split<'_, &str>) -> Vec<ChangelogEntry> {
+        let mut v = Vec::new();
+
+        while let Some(entry) = Commit::maybe_parse_changelog_entry(blocks) {
+            v.push(entry)
+        }
+
+        v
+    }
+
     /// This does NOT return a Result - if this fails, our tool is in the wrong and should panic.
     fn parse(input: String) -> Commit {
         // title is until two newlines
         // then message, until two newlines
         // then Changelog entries
 
-        let blocks: Vec<&str> = input.split("\n\n").collect();
+        // we split the commit message in blocks separated by two newlines.
+        // the first one is the title, the second one is the commit message, and then are the Changelog entries
+        let mut blocks = input.split("\n\n");
 
+        // there's always a title, otherwise we panic
+        let title = blocks.next().map(|s| Title(String::from(s))).unwrap();
+
+        // there might not be a commit message
+        let body = blocks.next().map(|s| Body(String::from(s)));
+
+        let entry = Commit::parse_changelog_entries(&mut blocks);
+
+        dbg!(title, body, entry);
         dbg!(blocks);
 
         todo!()
     }
 }
 
+#[derive(Parser)]
+struct Args {
+    #[arg(
+        short,
+        long,
+        help = "Commit message to unfrack. Generate using `git log -1 <sha> --format=%B`"
+    )]
+    input: String,
+}
+
 fn main() {
-    // TODO: Should we take the commit's body message as argument?
-
-    let input = Command::new("git")
-        .arg("log")
-        .arg("-1")
-        .arg("--format=%B")
-        .stdout(Stdio::piped())
-        .output()
-        .unwrap();
-    // FIXME: No unwrap
-
-    let commit = Commit::parse(String::from_utf8(input.stdout).unwrap());
+    let args = Args::parse();
+    let commit = Commit::parse(args.input);
 }
